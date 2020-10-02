566d7100a7dee93784d080ac45ad0c605241c303
Melanie Stuart <mmooradian90@yahoo.com>
2020-10-01 23:19:49 -0400

    colored sched added to JS for hours. need to work on future tenses.

--- script.js
+++ script.js
@@ -5,3 +5,17 @@
 $( "#currentDay" ).text(currentDay);
 
 console.log(moment().format("dddd, MMMM Do YYYY"))
+
+function colorSchedule(){
+    $("input").each(function(){
+        var rowHour = $(this).attr("id");
+        var rowNumber = parseInt(rowHour);
+        if (rowNumber === hour){
+            $(this).addClass("present");
+        } else if (rowNumber < hour){
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        };
+    });
+};
